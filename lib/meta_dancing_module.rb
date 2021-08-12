7b6669fe0f95cbf7220bb95eafb2e15a160a060b
Alec Magnet <amagnet@gmail.com>
2021-08-11 22:53:16 -0400

    complete

---
+++ lib/meta_dancing_module.rb
@@ -0,0 +1,13 @@
+module MetaDancing
+	def metadata
+		"This class produces objects that love to dance."
+	end
+
+	# class Dancer
+	# 	extend Metadancing
+	# end
+
+	# class Kid
+	# 	extend Metadancing
+	# end
+end
